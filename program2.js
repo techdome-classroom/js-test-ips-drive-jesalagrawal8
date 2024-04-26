37a6f7604885c1d93e4462b5caeb5d04f12fd630
jesalagrawal <jesal.agr11@gmail.com>
2024-04-26 08:35:35 +0100

    done

--- program2.js
+++ program2.js
@@ -1,7 +1,24 @@
 function longestSubstring(s) {
 
+    var n = s.length; 
+    var mx = 0; 
+    
+    for(var i = 0; i < n; i++) { 
+        var visited = new Array(256); 
+        for(var j = i; j < n; j++) { 
+            
+            if (visited[s.charAt(j)] == true) break;
+            else { 
+                mx = Math.max(mx, j - i + 1); 
+                visited[s.charAt(j)] = true; 
+            } 
+        } 
 
-    // Implementation of longestSubstring function
+        visited[s.charAt(i)] = false; 
+    } 
+    return mx; 
 }
+
+
 
 module.exports = { longestSubstring };
